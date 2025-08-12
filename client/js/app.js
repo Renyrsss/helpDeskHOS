825ffab152d778884e2592ddc9a57bc44bdfd97e
renys <renys_northside@mail.ru>
2025-08-12 09:20:34 +0500

    add kzd

--- client/js/app.js
+++ client/js/app.js
@@ -1,126 +1,117 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const token = '6515245927:AAExFk8USVwQ2IVcwtqszfutM-hqgbfp0Dg';
+    const token = "6515245927:AAExFk8USVwQ2IVcwtqszfutM-hqgbfp0Dg";
     let CHAT_ID = -4133903157;
-    const URI_API =  `https://api.telegram.org/bot${token}/sendMessage`;
-    let success = document.querySelector('.success');
-    let successImg = document.querySelector('.success__img');
-    let checkedOrNot = document.querySelector('.checkedOrNot');
-    let inputsRadio = document.querySelectorAll('.inputMar')
+    const URI_API = `https://api.telegram.org/bot${token}/sendMessage`;
+    let success = document.querySelector(".success");
+    let successImg = document.querySelector(".success__img");
+    let checkedOrNot = document.querySelector(".checkedOrNot");
+    let inputsRadio = document.querySelectorAll(".inputMar");
     let imageContainer = document.getElementsByTagName("body");
     let userDataQuery = getUsersQuery();
     // preloadImage("../img/image\ 1.svg","../img/image2.png","../img/image3.png", imageContainer[0]);
 
-    let radioInput = document.querySelectorAll('.radioInput');
+    let radioInput = document.querySelectorAll(".radioInput");
     // console.log(radioInput);
-    let inputs = document.querySelectorAll('.main__inputs');
-    let textArea = document.querySelector('textarea');
+    let inputs = document.querySelectorAll(".main__inputs");
+    let textArea = document.querySelector("textarea");
     // console.log(textArea);
-    let btn = document.querySelector('.btn__submit');
+    let btn = document.querySelector(".btn__submit");
     // console.log(btn);
     let admin = null;
-
-
-
-
-
-
-
-    btn.addEventListener('click',(e)=>{
+    let serverURl = "http://192.168.101.25:1338/api/zayavkis";
+    btn.addEventListener("click", (e) => {
         e.preventDefault();
-        let res = checkInputs(inputs,textArea,checkedOrNot,radioInput);
+        let res = checkInputs(inputs, textArea, checkedOrNot, radioInput);
         console.log(res);
-            if(res){
-                let query ;
-                radioInput.forEach(item=>{
-                    if(item.checked){
-                        query = item.value
+        if (res) {
+            let query;
+            radioInput.forEach((item) => {
+                if (item.checked) {
+                    query = item.value;
+                    console.log(item.id);
+
+                    if (item.id === "KZD") {
+                        serverURl = "http://192.168.101.25:1338/api/kzds";
+                        CHAT_ID = -4881074518;
                     }
-                })
-                console.log(checkedOrNot);
-                let massage = `<b>Заявка  ${query}</b>\n`;
-                massage +=  `<b>ФИО : ${inputs[1].value}</b>\n`;
-                massage +=  `<b>Отделение : ${inputs[2].value}</b>\n`;
-                massage +=  `<b>Телефон : ${inputs[0].value}</b>\n`
-                massage += `<b>Комментарий : ${textArea.value}</b>\n`;
-                massage += `<b>Запрос : ${query}</b>\n`;
-                axios.post(`http://192.168.101.25:1338/api/zayavkis`,{
-                    data:{
-                        userName : inputs[1].value,
-                        userPhone : inputs[0].value,
-                        userSide : inputs[2].value,
-                        userComment : textArea.value,
-                        userQuery : query
-                    }
-                
+                }
+            });
+            console.log(checkedOrNot);
+            let massage = `<b>Заявка  ${query}</b>\n`;
+            massage += `<b>ФИО : ${inputs[1].value}</b>\n`;
+            massage += `<b>Отделение : ${inputs[2].value}</b>\n`;
+            massage += `<b>Телефон : ${inputs[0].value}</b>\n`;
+            massage += `<b>Комментарий : ${textArea.value}</b>\n`;
+            massage += `<b>Запрос : ${query}</b>\n`;
+
+            const url = axios
+                .post(serverURl, {
+                    data: {
+                        userName: inputs[1].value,
+                        userPhone: inputs[0].value,
+                        userSide: inputs[2].value,
+                        userComment: textArea.value,
+                        userQuery: query,
+                    },
                 })
                 .then((res) => {
-                    inputs.forEach(item=>item.value = '');
-                    textArea.value = '';
-                    success.style.display = 'block';
-                    successImg.classList.add('successLoadingActive');
+                    inputs.forEach((item) => (item.value = ""));
+                    textArea.value = "";
+                    success.style.display = "block";
+                    successImg.classList.add("successLoadingActive");
 
-
-                    axios.post(URI_API,{
-                            chat_id:CHAT_ID,
-                            parse_mode: 'html',
-                            text:massage
+                    axios
+                        .post(URI_API, {
+                            chat_id: CHAT_ID,
+                            parse_mode: "html",
+                            text: massage,
                         })
-                        .then((res) => {
-                            
-                        })
-                        .catch((err) =>{
+                        .then((res) => {})
+                        .catch((err) => {
                             console.log(err);
-                        })
-
+                        });
 
                     setTimeout(() => {
-                        success.style.display = 'none'
-                        successImg.classList.remove('successLoadingActive');
-                        
+                        success.style.display = "none";
+                        successImg.classList.remove("successLoadingActive");
                     }, 3400);
                 })
-                .then(()=>{
-                    userDataQuery = '';
+                .then(() => {
+                    userDataQuery = "";
                     userDataQuery = getUsersQuery();
-                    
                 })
-                .then(()=>{
-                    queryListAdd()
+                .then(() => {
+                    queryListAdd();
                 })
-                .catch((err) =>{
+                .catch((err) => {
                     console.log(err);
-                })
-                
-
-
-            }
-    })
+                });
+        }
+    });
     // console.log(inputs);
-    let searchInput = document.querySelector('.main__searchInput');
+    let searchInput = document.querySelector(".main__searchInput");
 
     let userPhones = [];
-    let catalog = document.querySelector('.main__catalog');
-    function queryListAdd(){
-        userDataQuery.then(function(res){
-        // console.log(res);
-        searchInput.addEventListener('input',(e)=>{
-            catalog.innerHTML = ``;
-            userPhones = [];
-            res.forEach(item => {
-                console.log(item);
-            // console.log(item.userPhone);
-            if(item.userPhone.indexOf(e.target.value) === 0){
-                // console.log('true');
-                userPhones.push(item)
-            }
-            
-
-        })
-        // .slice(0,-14)
-        if(e.target.value.length >=5){
-            userPhones.forEach(item=>{
-                if(item.Progress == 'Сделано'){
-                    catalog.innerHTML += `
+    let catalog = document.querySelector(".main__catalog");
+    function queryListAdd() {
+        userDataQuery.then(function (res) {
+            // console.log(res);
+            searchInput.addEventListener("input", (e) => {
+                catalog.innerHTML = ``;
+                userPhones = [];
+                res.forEach((item) => {
+                    console.log(item);
+                    // console.log(item.userPhone);
+                    if (item.userPhone.indexOf(e.target.value) === 0) {
+                        // console.log('true');
+                        userPhones.push(item);
+                    }
+                });
+                // .slice(0,-14)
+                if (e.target.value.length >= 5) {
+                    userPhones.forEach((item) => {
+                        if (item.Progress == "Сделано") {
+                            catalog.innerHTML += `
                     <div class="main__catalogItem ">
                         <p class="main__catalogId"> ${item.userName}</p>
                         <p class="main__catalogDate">Дата ${item.createdAt}</p>
@@ -129,20 +120,25 @@ document.addEventListener("DOMContentLoaded", function () {
                         <p class="main__catalogItemProgress"><span class="main__catalogItemProgressbar greenDone">${item.Progress}</span></p>
                     </div>
                     `;
-                }
-                else if(item.Progress == 'в работе'){
-                    catalog.innerHTML += `
+                        } else if (item.Progress == "в работе") {
+                            catalog.innerHTML += `
                     <div class="main__catalogItem ">
                         <p class="main__catalogId"> ${item.userName}</p>
-                        <p class="main__catalogDate">Дата ${item.createdAt.slice(0,-14)}</p>
+                        <p class="main__catalogDate">Дата ${item.createdAt.slice(
+                            0,
+                            -14
+                        )}</p>
                         <p class="main__catalogItemName">${item.userQuery}</p>
-                        <p class="main__catalogItemComment">${item.userComment}</p>
-                        <p class="main__catalogItemProgress"><span class="main__catalogItemProgressbar yellowInWork">${item.Progress}</span></p>
+                        <p class="main__catalogItemComment">${
+                            item.userComment
+                        }</p>
+                        <p class="main__catalogItemProgress"><span class="main__catalogItemProgressbar yellowInWork">${
+                            item.Progress
+                        }</span></p>
                     </div>
                     `;
-                }
-                else if (item.Progress == 'Новая заявка'){
-                    catalog.innerHTML += `
+                        } else if (item.Progress == "Новая заявка") {
+                            catalog.innerHTML += `
                     <div class="main__catalogItem ">
                         <p class="main__catalogId"> ${item.userName}</p>
                         <p class="main__catalogDate">Дата ${item.createdAt}</p>
@@ -151,92 +147,74 @@ document.addEventListener("DOMContentLoaded", function () {
                         <p class="main__catalogItemProgress"><span class="main__catalogItemProgressbar blueNewQuery">${item.Progress}</span></p>
                     </div>
                     `;
+                        }
+                    });
                 }
-        })
-        }
-    })
-        
-    })
-}
+            });
+        });
+    }
 
-    
     queryListAdd();
 
-
-
-
-
     // ------------------------------------
-
-
-
-
 });
 
-
-
-
-function checkInputs(inputs,textArea,checkedOrNot,inputsRadio){
-    let res = true
-    inputs.forEach(element => {
-        if(element.value.trim() == ''){
-            console.log('error');
+function checkInputs(inputs, textArea, checkedOrNot, inputsRadio) {
+    let res = true;
+    inputs.forEach((element) => {
+        if (element.value.trim() == "") {
+            console.log("error");
             element.style.cssText = `
             border:2px solid red
             `;
-            res = false
+            res = false;
+        } else {
+            element.style.cssText = `none`;
         }
-        else{
-            element.style.cssText = `none`
-        }
-        
     });
 
-    if(textArea.value.trim() == ''){
-        console.log('error');
+    if (textArea.value.trim() == "") {
+        console.log("error");
         textArea.style.cssText = `border:2px solid red`;
-        res = false
-    }
-    else{
-        textArea.style.cssText = `none`
+        res = false;
+    } else {
+        textArea.style.cssText = `none`;
     }
 
     console.log(inputsRadio);
     let radioFalse = false;
-    inputsRadio.forEach(item =>{
-        if(item.checked == false){
+    inputsRadio.forEach((item) => {
+        if (item.checked == false) {
             console.log(item.checked);
+        } else if (item.checked) {
+            radioFalse = true;
         }
-        else if(item.checked){
-            radioFalse = true
-        }
-    })
-    if(radioFalse){
+    });
+    if (radioFalse) {
         checkedOrNot.style.cssText = ``;
-    }
-    else{
+    } else {
         checkedOrNot.style.cssText = `border:4px solid red`;
-        res = false
+        res = false;
     }
     return res;
 }
 
+async function getUsersQuery() {
+    let userObj = [];
 
-async function getUsersQuery(){
-
-            let userObj = [];
-
-            await axios.get('http://192.168.101.25:1338/api/zayavkis?pagination[pageSize]=1000&sort=createdAt:desc').then(function(res){
-                res.data['data'].map(item => {
-                    userObj.push(item.attributes)
-                })
+    await axios
+        .get(
+            "http://192.168.101.25:1338/api/zayavkis?pagination[pageSize]=1000&sort=createdAt:desc"
+        )
+        .then(function (res) {
+            res.data["data"].map((item) => {
+                userObj.push(item.attributes);
             });
+        });
 
-
-            // console.log(userObj);
-        return userObj;
+    // console.log(userObj);
+    return userObj;
 }
-
 
 // async function changeCotalog(items){
 //     let changeCatalog = document.querySelector('.main_catalogItem');
